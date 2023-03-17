f6676102cd452c782ea5dca6d3fd6a4a16d713aa
Chirag Sharma <chiragsharma@Chirags-MacBook-Pro.local>
2023-03-17 23:00:41 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const { parse } = require('csv-parse');
+const fs = require('fs');
+
+const result = [];
+
+fs.createReadStream('kepler_data.csv')
+  .pipe(
+    parse({
+      comment: '#',
+      columns: true,
+    })
+  )
+  .on('data', (data) => {
+    result.push(data);
+  })
+  .on('error', (err) => {
+    console.log(err);
+  })
+  .on('end', () => {
+    console.log(result);
+    console.log('DONEEE ----------  EOF');
+  });
